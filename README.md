10349503859cbb78c6db542537fdfbbcd8f23151
maxgdev <max@maxgdev.net>
2021-02-28 19:12:36 +0000

    Added earthquake_model.dart cobtained EarthQuake model

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ Flutter US Earthquake Mapping App sung Google Maps
 
 ## Credits
 - [Packt Publishing] (https://www.packtpub.com/)
+- https://earthquake.usgs.gov/earthquakes
 - https://app.quicktype.io/
 - https://jsonformatter.curiousconcept.com/
 - http://jsonplaceholder.typicode.com/
-- https://fontawesome.com/
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
 - [online documentation](https://flutter.dev/docs), which offers tutorials,
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
-// import './components/maps_page.dart';
 import './components/simple_map.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Earthquake Mapping App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      // home: MyHomePage(title: 'Earthquake Mapping App '),
       home: SimpleMap(title:"Earthquake Mapping App"),
     );
   }
