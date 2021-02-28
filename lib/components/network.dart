33f744ead0a814f2d8f84467df3a37d413c46a7c
maxgdev <max@maxgdev.net>
2021-02-28 20:00:45 +0000

    Added network.dart to get JSON data

---
+++ lib/components/network.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import './earthquake_model.dart';
+
+class Network {
+
+  Future<EarthQuake> getAllQuakes() async {
+     var apiUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+
+     final response = await get(Uri.encodeFull(apiUrl));
+      print("EarthQuake 'response': $response");
+      
+     if (response.statusCode == 200) {
+        print("EarthQuake data: ${response.body}");
+        return EarthQuake.fromJson(json.decode(response.body));
+     }else {
+        throw Exception("Error getting quakes");
+     }
+
+  }
+}
